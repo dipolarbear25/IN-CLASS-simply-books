8c8a9e84ccde4e36d1483151396cc87784dce165
Austin Mangum <mangumaustin@gmail.com>
2023-10-14 12:24:11 -0500

    implementing CRUD and many to many relationships

--- components/forms/AuthorForm.js
+++ components/forms/AuthorForm.js
@@ -1,31 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { createAuthor, updateAuthor } from '../../api/authorData';
 import { useAuth } from '../../utils/context/authContext';
-import { createAuthor, updateAuthor, getAuthors } from '../../api/authorData';
 
+// Setting Initial state for useState()
 const initialState = {
-  image: '',
+  email: '',
   favorite: false,
   first_name: '',
   last_name: '',
 };
 
-function AuthorForm({ obj }) {
-  console.warn(obj);
+export default function AuthorForm({ obj }) {
   const [formInput, setFormInput] = useState(initialState);
-  const [authors, setAuthors] = useState([]);
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
-    getAuthors(user.uid).then(setAuthors);
-
     if (obj.firebaseKey) setFormInput(obj);
-  }, [obj, user]);
+  }, [obj]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -38,13 +34,13 @@ function AuthorForm({ obj }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj.firebaseKey) {
-      updateAuthor(formInput).then(() => router.push('/authors'));
+      updateAuthor(formInput).then(() => router.push(`/author/${obj.firebaseKey}`));
     } else {
       const payload = { ...formInput, uid: user.uid };
       createAuthor(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
         updateAuthor(patchPayload).then(() => {
-          router.push('/authors');
+          router.push('/');
         });
       });
     }
@@ -54,75 +50,56 @@ function AuthorForm({ obj }) {
     <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Author</h2>
 
-      {/* NAME INPUT  */}
-      <FloatingLabel controlId="floatingInput1" label="Author First Name" className="mb-2">
+      <Form.Group className="mb-3 text-white mt-5" controlId="authorFirstName">
+        <Form.Label>First Name</Form.Label>
         <Form.Control
           type="text"
-          placeholder="Enter Author Name"
+          placeholder="First Name"
           name="first_name"
           value={formInput.first_name}
           onChange={handleChange}
           required
         />
-      </FloatingLabel>
+      </Form.Group>
 
-      <FloatingLabel controlId="floatingInput1" label="Author Last Name" className="mb-2">
-        <Form.Control
-          type="text"
-          placeholder="Enter Author Name"
-          name="last_name"
-          value={formInput.last_name}
-          onChange={handleChange}
-          required
+      <Form.Group className="mb-3 text-white mt-5" controlId="authorLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder="Last Name" name="last_name" value={formInput.last_name} onChange={handleChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3 text-white mt-5" controlId="authorEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" name="email" value={formInput.email} onChange={handleChange} required />
+      </Form.Group>
+      <Form.Group className="mb-3 text-white mt-5" controlId="authorFavorite">
+        <Form.Check
+          type="checkbox"
+          label="Favorite?"
+          name="favorite"
+          checked={formInput.favorite}
+          onChange={(e) => {
+            setFormInput((prevState) => ({
+              ...prevState,
+              favorite: e.target.checked,
+            }));
+          }}
         />
-      </FloatingLabel>
-
-      {/* IMAGE INPUT  */}
-      <FloatingLabel controlId="floatingInput2" label="Author Image" className="mb-2">
-        <Form.Control
-          type="url"
-          placeholder="Enter an image url"
-          name="image"
-          value={formInput.image}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
-
-      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
-      <Form.Check
-        className="text-white mb-3"
-        type="switch"
-        id="favorite"
-        name="favorite"
-        label="favorite this author?"
-        checked={formInput.favorite}
-        onChange={(e) => {
-          setFormInput((prevState) => ({
-            ...prevState,
-            favorite: e.target.checked,
-          }));
-        }}
-      />
-
-      {/* SUBMIT BUTTON  */}
-      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Author</Button>
+      </Form.Group>
+      <Button variant="primary" type="submit"> {obj.firebaseKey ? 'Update' : 'Create'} Author
+      </Button>
     </Form>
   );
 }
 
 AuthorForm.propTypes = {
   obj: PropTypes.shape({
-    image: PropTypes.string,
+    email: PropTypes.string,
     favorite: PropTypes.bool,
+    firebaseKey: PropTypes.string,
     first_name: PropTypes.string,
     last_name: PropTypes.string,
-    firebaseKey: PropTypes.string,
   }),
 };
 
 AuthorForm.defaultProps = {
   obj: initialState,
 };
-
-export default AuthorForm;
